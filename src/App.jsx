1e3dcfad5808a9819dd9b2ed3d6853d5dc2c1be1
thatgeekyboii <vaibhav0509.patil@gmail.com>
2025-06-23 08:54:06 +0530

    initial files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import OTPgenerator from './components/OTPgenerator'
+
+function App() {
+
+  return (
+    <>
+    <OTPgenerator></OTPgenerator>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/OTPgenerator.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OTPgenerator = () => {
+  return (
+    <div>
+      <h1>OTP generator</h1>
+    </div>
+  )
+}
+
+export default OTPgenerator
